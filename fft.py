952c4c33d7da2c246e82e32b17e677fbc09bb54f
Nikhil Pai <zirconikhil@gmail.com>
2022-05-10 16:38:22 +0200

    Updated with recording

--- fft.py
+++ fft.py
@@ -1,13 +1,41 @@
+import sounddevice as sd
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
 import numpy as np
 
-def DFT(x):
-    """
-    Compute the discrete Fourier Transform of the 1D array x
-    :param x: (array)
-    """
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
 
-    N = x.size
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    e = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(e, x)
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()
+print('Finished recording')
+
+plt.plot(myrecording)
+plt.ylabel('Sound')
+plt.xlabel('Time')
+plt.show()
+
+t = np.linspace(0, seconds, 500)
+s = myrecording
+
+# plt.ylabel("Amplitude")
+# plt.xlabel("Time [s]")
+# plt.plot(t, s)
+# plt.show()
+
+fft = np.fft.fft(s)
+
+for i in range(seconds):
+    print("Value at index {}:\t{}".format(i, fft[i + 1]), "\nValue at index {}:\t{}".format(fft.size -1 - i, fft[-1 - i]))
+    
+fft = np.fft.fft(s)
+T = t[1] - t[0]  # sampling interval 
+N = s.size
+
+# 1/T = frequency
+f = np.linspace(0, 1 / T, N)
+
+plt.ylabel("Amplitude")
+plt.xlabel("Frequency [Hz]")
+plt.bar(f[:N // 2], np.abs(fft)[:N // 2] * 1 / N, width=1.5)  # 1 / N is a normalization factor
+plt.show()
