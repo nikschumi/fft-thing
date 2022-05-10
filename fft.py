e0ec3ca4a1acc47358beccd8e969ebc9bd7034a1
Nikhil Pai <zirconikhil@gmail.com>
2022-05-10 16:25:15 +0200

    Create fft.py

---
+++ fft.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def DFT(x):
+    """
+    Compute the discrete Fourier Transform of the 1D array x
+    :param x: (array)
+    """
+
+    N = x.size
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    e = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(e, x)
